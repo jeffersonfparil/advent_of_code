b694c2b2f1781328924937e242731c56c23fccb0
jeff <jeffersonparil@gmail.com>
2021-12-03 17:47:29 +1100

    day 03 not finished yet

--- 2021/aoc-2021-03-code.jl
+++ 2021/aoc-2021-03-code.jl
@@ -3,11 +3,8 @@ using DelimitedFiles
 fname = "aoc-2021-03-input.txt"
 dat = DelimitedFiles.readdlm(fname, '\t', AbstractString, header=false)
 X = parse.(Int, hcat(split.(dat, "")[:,1]...))'
-
 gamma = parse(Int64, string.(digits.(sum(X, dims=1) .> (size(X)[1]/2))...)[1], base=2)
 epsilon = parse(Int64, string.(digits.(sum(X, dims=1) .<= (size(X)[1]/2))...)[1], base=2)
-
-
 
 println("#####################################")
 println("Part 1:")
