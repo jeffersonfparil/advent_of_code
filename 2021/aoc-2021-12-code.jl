81c43c073c7bd6dd631cab2f5233d6b9e1b43665
jeffersonfparil <jeffersonparil@gmail.com>
2021-12-14 00:12:20 +1100

    finished day 13

--- 2021/aoc-2021-12-code.jl
+++ 2021/aoc-2021-12-code.jl
@@ -1,21 +1,4 @@
 ### NOTE: We're assuming no two big caves are directly connected!
-
-fname = "aoc-2021-12-input.txt"
-mat_str_caves = hcat(split.(readlines(fname), "-")...)
-# mat_str_caves = hcat(split.(["dc-end", "HN-start", "start-kj", "dc-start", "dc-HN", "LN-dc", "HN-end", "kj-sa", "kj-HN", "kj-dc"], "-")...)
-# mat_str_caves = hcat(split.(["fs-end", "he-DX", "fs-he", "start-DX", "pj-DX", "end-zg", "zg-sl", "zg-pj", "pj-he", "RW-he", "fs-DX", "pj-RW", "zg-RW", "start-pj", "he-WI", "zg-he", "pj-fs", "start-RW"], "-")...)
-
-vec_caves_list_all = unique(mat_str_caves)
-vec_caves_list_uppercase = vec_caves_list_all[[uppercase(x)==x for x in vec_caves_list_all]]
-vec_caves_list_lowercase = vec_caves_list_all[[uppercase(x)!=x for x in vec_caves_list_all]]
-vec_caves_list_lowercase = vec_caves_list_lowercase[.!in.(vec_caves_list_lowercase, [["start", "end"]])]
-
-idx_starting_caves = collect(1:size(mat_str_caves,2))[sum(mat_str_caves .== "start", dims=1)[1,:] .== 1]
-idx_nonstarting_caves = collect(1:size(mat_str_caves,2))[sum(mat_str_caves .!= "start", dims=1)[1,:] .== 2]
-mat_str_starting_caves = mat_str_caves[:, idx_starting_caves]
-mat_str_nonstarting_caves = mat_str_caves[:, idx_nonstarting_caves]
-
-
 fun_cave_crawler = function(vec_vec_path, mat_str_nonstarting_caves, vec_caves_list_lowercase; part1=true)
     bool_have_we_not_reached_the_end_cave = (vec_vec_path[end][end] != "end")
     if part1
@@ -25,7 +8,7 @@ fun_cave_crawler = function(vec_vec_path, mat_str_nonstarting_caves, vec_caves_l
         vec_n_lowercase_caves_in_our_path = [count(vec_vec_path[end].==x) for x in vec_caves_list_lowercase]
         bool_do_we_have_lowercase_caves_appearing_more_than_twice = sum(vec_n_lowercase_caves_in_our_path .> 2) > 0
         bool_do_we_have_at_most_one_lowercase_cave_apearing_twice = sum(vec_n_lowercase_caves_in_our_path .== 2) <= 1
-        bool_test = (bool_do_we_have_lowercase_caves_appearing_more_than_twice==false) & (bool_do_we_have_at_most_one_lowercase_cave_apearing_twice==true)
+        bool_test = (bool_have_we_not_reached_the_end_cave) & (bool_do_we_have_lowercase_caves_appearing_more_than_twice==false) & (bool_do_we_have_at_most_one_lowercase_cave_apearing_twice==true)
     end
     # println(vec_vec_path)
     # println(bool_do_we_have_at_most_one_lowercase_cave_in_our_path)
@@ -61,17 +44,44 @@ fun_cave_crawler = function(vec_vec_path, mat_str_nonstarting_caves, vec_caves_l
     return(vec_vec_path)
 end
 
-part1 = false
-vec_vec_all_paths = []
-for i in idx_starting_caves
-    # i = idx_starting_caves[1]
-    vec_vec_path = [[mat_str_caves[mat_str_caves[:,i] .== "start", i][1], mat_str_caves[mat_str_caves[:,i] .!= "start", i][1]]]
-    X = fun_cave_crawler(vec_vec_path, mat_str_nonstarting_caves, vec_caves_list_lowercase, part1=part1)[1:(end-1)]
-    vec_vec_all_paths = vcat(vec_vec_all_paths, X)
+fun_find_all_paths = function(fname; part1=true)
+    mat_str_caves = hcat(split.(readlines(fname), "-")...)
+    # mat_str_caves = hcat(split.(["dc-end", "HN-start", "start-kj", "dc-start", "dc-HN", "LN-dc", "HN-end", "kj-sa", "kj-HN", "kj-dc"], "-")...)
+    # mat_str_caves = hcat(split.(["fs-end", "he-DX", "fs-he", "start-DX", "pj-DX", "end-zg", "zg-sl", "zg-pj", "pj-he", "RW-he", "fs-DX", "pj-RW", "zg-RW", "start-pj", "he-WI", "zg-he", "pj-fs", "start-RW"], "-")...)
+    # part1 = false
+
+    vec_caves_list_all = unique(mat_str_caves)
+    vec_caves_list_uppercase = vec_caves_list_all[[uppercase(x)==x for x in vec_caves_list_all]]
+    vec_caves_list_lowercase = vec_caves_list_all[[uppercase(x)!=x for x in vec_caves_list_all]]
+    vec_caves_list_lowercase = vec_caves_list_lowercase[.!in.(vec_caves_list_lowercase, [["start", "end"]])]
+
+    idx_starting_caves = collect(1:size(mat_str_caves,2))[sum(mat_str_caves .== "start", dims=1)[1,:] .== 1]
+    idx_nonstarting_caves = collect(1:size(mat_str_caves,2))[sum(mat_str_caves .!= "start", dims=1)[1,:] .== 2]
+    mat_str_starting_caves = mat_str_caves[:, idx_starting_caves]
+    mat_str_nonstarting_caves = mat_str_caves[:, idx_nonstarting_caves]
+    vec_vec_all_paths = []
+    for i in idx_starting_caves
+        # i = idx_starting_caves[1]
+        vec_vec_path = [[mat_str_caves[mat_str_caves[:,i] .== "start", i][1], mat_str_caves[mat_str_caves[:,i] .!= "start", i][1]]]
+        X = fun_cave_crawler(vec_vec_path, mat_str_nonstarting_caves, vec_caves_list_lowercase, part1=part1)[1:(end-1)]
+        vec_vec_all_paths = vcat(vec_vec_all_paths, X)
+    end
+    if part1
+        println("#####################################")
+        println("Part 1:")
+        println(length(vec_vec_all_paths))
+        println("#####################################")
+    else
+        println("#####################################")
+        println("Part 2:")
+        println(length(vec_vec_all_paths))
+        println("#####################################")
+    end
 end
 
-println("#####################################")
-println("Part 1:")
-println(length(vec_vec_all_paths))
-println("#####################################")
 
+fname = "aoc-2021-12-input.txt"
+@time fun_find_all_paths(fname)
+@time fun_find_all_paths(fname)
+@time fun_find_all_paths(fname, part1=false)
+@time fun_find_all_paths(fname, part1=false)
---
+++ 2021/aoc-2021-13-code.jl
@@ -0,0 +1,67 @@
+using UnicodePlots
+
+fun_parse = function(vec_input)
+    ### dot coordinates
+    vec_x = []
+    vec_y = []
+    i = 1
+    while vec_input[i] != ""
+        line = vec_input[i]
+        vec_coordinates = parse.(Int, split(line, ","))
+        append!(vec_x, vec_coordinates[1])
+        append!(vec_y, vec_coordinates[2])
+        i += 1
+    end
+    ### convert from 0-index into 1-index
+    vec_x = vec_x .+ 1
+    vec_y = vec_y .+ 1
+    ### folds
+    vec_folds = []
+    for j in (i+1):length(vec_input)
+        # j = i + 1
+        line = vec_input[j]
+        push!(vec_folds, string(split(line, " ")[end]))
+    end
+    return(hcat(vec_x, vec_y), vec_folds)
+end
+
+fun_fold_once_or_fold_and_read_message = function(fname; part1=true)
+    vec_input = readlines(fname)
+    # vec_input = ["6,10","0,14","9,10","0,3","10,4","4,11","6,0","6,12","4,1","0,13","10,12","3,4","3,0","8,4","1,10","2,14","8,10","9,0","","fold along y=7","fold along x=5"]
+    # part1 = false
+    mat_coordinates, vec_folds = fun_parse(vec_input)
+    if part1
+        vec_folds = [vec_folds[1]]
+    end
+    for str_fold in vec_folds
+        # str_fold = vec_folds[1]
+        vec_str_fold = split(str_fold, "=")
+        str_axis_fold = vec_str_fold[1]
+        int_coordinate_fold = parse(Int, vec_str_fold[end]) + 1 ### Convert from 0-index into 1-index
+        if str_axis_fold == "y"
+            vec_bool_coordinates_below_fold_line = mat_coordinates[:, 2] .> int_coordinate_fold
+            mat_coordinates[vec_bool_coordinates_below_fold_line, 2] = int_coordinate_fold .- (mat_coordinates[vec_bool_coordinates_below_fold_line, 2] .- int_coordinate_fold)
+        else
+            vec_bool_coordinates_below_fold_line = mat_coordinates[:, 1] .> int_coordinate_fold
+            mat_coordinates[vec_bool_coordinates_below_fold_line, 1] = int_coordinate_fold .- (mat_coordinates[vec_bool_coordinates_below_fold_line, 1] .- int_coordinate_fold)
+        end
+    end
+    if part1
+        println("#####################################")
+        println("Part 1:")
+        vec_str_folded = [string(mat_coordinates[i, 1], "-", mat_coordinates[i, 2]) for i in 1:size(mat_coordinates,1)]
+        println(length(unique(vec_str_folded)))
+        println("#####################################")
+    else
+        println("#####################################")
+        println("Part 2:")
+        println(UnicodePlots.scatterplot(mat_coordinates[:,1], .-mat_coordinates[:,2], width=100))
+        println("#####################################")
+    end
+end
+        
+fname = "aoc-2021-13-input.txt"
+@time fun_fold_once_or_fold_and_read_message(fname)
+@time fun_fold_once_or_fold_and_read_message(fname)
+@time fun_fold_once_or_fold_and_read_message(fname, part1=false)
+@time fun_fold_once_or_fold_and_read_message(fname, part1=false)
