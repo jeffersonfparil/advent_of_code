4608ba4fac9e7a7a603e7cdc0c100d6f80670b02
jeffersonfparil <jeffersonparil@gmail.com>
2022-12-01 16:47:56 +1100

    trying to learn rust LOL

--- 2022/target/debug/advent_of_code_2022.d
+++
@@ -1,1 +0,0 @@
-/home/jeffersonfparil/Documents/advent_of_code/2022/target/debug/advent_of_code_2022: /home/jeffersonfparil/Documents/advent_of_code/2022/src/main.rs
--- 2022/target/debug/deps/advent_of_code_2022-2b641bd85b602a54.d
+++
@@ -1,5 +0,0 @@
-/home/jeffersonfparil/Documents/advent_of_code/2022/target/debug/deps/advent_of_code_2022-2b641bd85b602a54.rmeta: src/main.rs
-
-/home/jeffersonfparil/Documents/advent_of_code/2022/target/debug/deps/advent_of_code_2022-2b641bd85b602a54.d: src/main.rs
-
-src/main.rs:
