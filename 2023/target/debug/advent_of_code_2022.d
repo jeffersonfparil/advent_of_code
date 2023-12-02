37f9e7e2ae7815cb57761cb11a00b930ce447571
jeffersonfparil <jeffersonparil@gmail.com>
2023-12-02 22:08:38 +1100

    2023 day 1 part 1

--- 2023/target/debug/advent_of_code_2022.d
+++
@@ -1,1 +0,0 @@
-/home/jeffparil/Documents/advent_of_code/2023/target/debug/advent_of_code_2022: /home/jeffparil/Documents/advent_of_code/2023/src/day01.rs /home/jeffparil/Documents/advent_of_code/2023/src/main.rs
--- 2023/target/debug/deps/advent_of_code_2022-43a30232a759a72b.d
+++
@@ -1,6 +0,0 @@
-/home/jeffparil/Documents/advent_of_code/2023/target/debug/deps/advent_of_code_2022-43a30232a759a72b.rmeta: src/main.rs src/day01.rs
-
-/home/jeffparil/Documents/advent_of_code/2023/target/debug/deps/advent_of_code_2022-43a30232a759a72b.d: src/main.rs src/day01.rs
-
-src/main.rs:
-src/day01.rs:
